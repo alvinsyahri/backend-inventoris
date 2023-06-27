ec1e59d90eceffec87a8c510b8d02f128d3998d5
alvinsyahri <alvinsyahri080@gmail.com>
2023-06-27 21:59:36 +0700

    crash gak jelas karena enter

--- controller/dashboard/peminjamanController.js
+++ controller/dashboard/peminjamanController.js
@@ -25,7 +25,9 @@ module.exports = {
                 userId,
                 barangId
             };
+
             const peminjaman = await Peminjaman.create(newPeminjaman);
+            
             const barang = await Barang.findOne({ _id: barangId});
             barang.status = true;
             barang.peminjamanId.push({ _id: peminjaman._id});
@@ -46,16 +48,19 @@ module.exports = {
             const updatedAt = new Date()
             const peminjaman =  await Peminjaman.findOne({ _id: id})
             const updateBarangId = peminjaman.barangId;
+
             peminjaman.keterangan = keterangan;
             peminjaman.userId = userId;
             peminjaman.barangId = barangId;
             peminjaman.updatedAt = updatedAt;
             await peminjaman.save();
+
             const barang = await Barang.findOne({ _id: updateBarangId})
             barang.status = false;
             barang.save();
+
             const barangUpdate = await Barang.findOne({ _id: barangId})
-            barangUpdate.status = false;
+            barangUpdate.status = true;
             barangUpdate.save();
             res.status(200).json({
                 'status' : "SuccesS Edit"
@@ -71,26 +76,22 @@ module.exports = {
         try {
             const { id } = req.params;
             const peminjaman = await Peminjaman.findOne({ "_id": id })
-            // Temukan kategori yang memiliki peminjaman dengan ID yang sesuai
             const barang = await Barang.findOne({ "_id": peminjaman.barangId });
             
             if (barang) {
-            // Temukan index peminjaman yang ingin dihapus
             const indexToDelete = barang.peminjamanId.findIndex(
                 (peminjaman) => peminjaman._id.toString() === id
             );
                 if (indexToDelete !== -1) {
                     barang.peminjamanId.splice(indexToDelete, 1);
                     await barang.save();
-                    // Hapus peminjaman dari koleksi peminjaman
                     await Peminjaman.deleteOne({ _id: id });
-
                     res.status(200).json({
-                    status: "Berhasil Menghapus peminjaman",
+                        status: "Berhasil Menghapus peminjaman",
                     });
                 } else {
                     res.status(404).json({
-                    status: "peminjaman Tidak Ditemukan",
+                        status: "peminjaman Tidak Ditemukan",
                     });
                 }
             } else {
