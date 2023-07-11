7ae9fd6f02f70a0ab483b3e74b36ae5b070044c6
alvinsyahri <alvinsyahri080@gmail.com>
2023-07-11 14:57:50 +0700

    dashboard

--- controller/dashboard/indexController.js
+++ controller/dashboard/indexController.js
@@ -1,12 +1,28 @@
+const Item = require('../../model/Item')
 
 module.exports = {
 
     viewDashboard : async(req, res) => {
         try {
+            const data = await Item.find()
+            .sort({ condition: -1 })
+            .populate({
+                path: 'subCategoryId',
+                select: 'name',
+                populate: {
+                    path: 'categoryId',
+                    select: 'name'
+                }
+            })
+            let card = []
+            card.push(await Item.countDocuments({ qty: { $gt: 1 }}))
+            card.push(await Item.countDocuments({ qty: 0}))
             res.status(200).json({
                 'status' : "Success",
                 'valid': true,
-                'username': req.username
+                'username': req.username,
+                'data': data,
+                'card': card
             });
         } catch (error) {
             res.status(400).json({
